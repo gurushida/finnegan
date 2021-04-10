e798df5d4cf9f7424439387e64537ba0c34df25b
gurushida <49831684+gurushida@users.noreply.github.com>
2021-04-10 23:25:37 +0200

    Updated dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,6 +3,9 @@
 # This is the library that performs the voice recognition
 pip3 install vosk
 
+# This is the library used to listen from audio input devices
+pip3 install sounddevice
+
 # This is the data model used to recognize English speech
 curl https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip -o vosk-model-small-en-us-0.15.zip
 
