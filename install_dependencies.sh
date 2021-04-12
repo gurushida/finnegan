5df2248984f0f6fe6bdb2baf4b860998feb5d03e
gurushida <49831684+gurushida@users.noreply.github.com>
2021-04-12 22:57:55 +0200

    Downloading data for French as well

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,8 +9,11 @@ pip3 install sounddevice
 # This is the data model used to recognize English speech
 curl https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip -o vosk-model-small-en-us-0.15.zip
 
-unzip vosk-model-small-en-us-0.15.zip
+# This is the data model used to recognize French speech
+curl https://alphacephei.com/vosk/models/vosk-model-small-fr-pguyot-0.3.zip -o vosk-model-small-fr-pguyot-0.3.zip
 
-rm -rf model
-mv vosk-model-small-en-us-0.15 model
-rm vosk-model-small-en-us-0.15.zip
+rm -rf vosk-model-small-en-us-0.15 vosk-model-small-fr-pguyot-0.3
+unzip vosk-model-small-en-us-0.15.zip
+unzip vosk-model-small-fr-pguyot-0.3.zip
+
+rm vosk-model-small-en-us-0.15.zip vosk-model-small-fr-pguyot-0.3.zip
